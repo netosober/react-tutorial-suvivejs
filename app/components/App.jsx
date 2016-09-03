8346a9b59bfaadaba772152785a1e68affc96090
Ernesto Soberanes <ernesto@toptal.com>
2016-09-03 11:26:23 -0700

    Chapter 8 React and Flux

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
+import connect from '../libs/connect';
 
-export default class App extends React.Component {
-  constructor(props) {
+class App extends React.Component {
+    constructor(props) {
     super(props);
 
     this.state = {
@@ -24,6 +25,7 @@ export default class App extends React.Component {
 
     return (
       <div>
+        {this.props.test}
         <button className="add-note" onClick={this.addNote}>+</button>
         <Notes
           notes={notes}
@@ -73,3 +75,7 @@ export default class App extends React.Component {
     });
   }
 }
+
+export default connect(() => ({
+  test: 'test'
+}))(App)
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default ({children, ...props}) => (
-    <div {...props}>
+  <div {...props}>
       {children}
-    </div>
+  </div>
 );
