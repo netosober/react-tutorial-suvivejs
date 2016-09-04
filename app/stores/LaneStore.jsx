b83024418181760f05a136aea41bebdd13169278
Ernesto Soberanes <netosober@gmail.com>
2016-09-04 12:50:13 -0700

    Chapter 13 Editing Lanes

--- app/stores/LaneStore.jsx
+++ app/stores/LaneStore.jsx
@@ -35,4 +35,19 @@ export default class LaneStore {
       })
     });
   }
+  update(updatedLane) {
+    this.setState({
+      lanes: this.lanes.map(lane => {
+        if (lane.id === updatedLane.id) {
+          return Object.assign({}, lane, updatedLane);
+        }
+        return lane;
+      })
+    });
+  }
+  delete(id) {
+    this.setState({
+      lanes: this.lanes.filter(lane => lane.id !== id)
+    });
+  }
 }
